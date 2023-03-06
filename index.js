c6ac8459915f938e1497b84dc8c4fac7b91dd37f
tobedoit <tobedoit@aol.com>
2023-03-07 02:23:08 +0900

    edit

--- index.js
+++ index.js
@@ -12,7 +12,8 @@ dotenv.config();
 
 const app = express();
 
-app.use(express.static(path.join(__dirname, 'dist')));
+app.use(express.static('dist'));
+
 app.use(cors());
 // app.use(express.urlencoded({ extended: false }));
 app.use(express.json({ limit: '50mb' }));
